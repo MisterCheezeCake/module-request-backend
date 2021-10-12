2293bf6db4a634ec2a00fcb8937fb526a936a6d8
MisterCheezeCake <zwishod@gmail.com>
2021-10-11 22:00:40 -0400

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Module Request Backend
+
+This is a backend designed to act as an intermediary between a Minecraft Mod or ChatTriggers module and a discord webhook. 
+
+## Dependencies
+
+[express](http://expressjs.com/) - Network
+[express-rate-limit](https://github.com/nfriedly/express-rate-limit) - Rate Limiting
+[discord.js](https://discord.js.org/#/) - Discord API
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const config = require("./config.json")
+const { WebhookClient, MessageEmbed } = require("discord.js")
+const client = new WebhookClient({url: config.webhook})
+const rateLimit = require("express-rate-limit");
+const app = express()
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 1
+});
+app.use(limiter)
+app.use(express.json())
+app.post('/', (req, res) => {
+  if (!req.body.text || !req.body.uuid) {
+    res.send("You must provide both text and a UUID, also it looks like you are requesting the backend outside the mod, please dont do that.")
+    return;
+  }
+  const {text, uuid} = req.body
+  const module_ = req.body.module || "Unspecified"
+  let color = Math.round(Math.random() * 16777215)
+  if (color > 16777215 || color < 0) color = 0
+  client.send({embeds: [new MessageEmbed().setTitle("Request").addField("User", `\`${uuid}\``).addField("Module", module_).setDescription(text).setColor(color)]}).then(m => {
+    res.send("Request Complete")
+  })
+})
+
+
+app.listen(8080, () => console.log('App on localhost:8080') )
